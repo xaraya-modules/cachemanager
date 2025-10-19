49322e3c8d2cadcba897fa07ffd9bfb6673ac9ed
mikespub <github@mikespub.net>
2025-10-19 18:38:25 +0200

    add tables

---
+++ tables.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * CacheManager table setup
+ *
+ * @package modules
+ * @copyright (C) 2002-2009 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage CacheManager module
+ * @link http://xaraya.com/index.php/release/1652.html
+ * @author jsb
+*/
+
+namespace Xaraya\Modules\CacheManager;
+
+use xarDB;
+
+class Tables
+{
+    /**
+     * Return cache tables
+     * @return array
+     */
+    public function __invoke(?string $prefix = null)
+    {
+        // Initialise table array
+        $xartable = [];
+        $prefix ??= xarDB::getPrefix();
+
+        // Set the table names
+        $xartable['cache_blocks'] = $prefix . '_cache_blocks'; // cfr. blocks module
+        $xartable['cache_data'] = $prefix . '_cache_data';
+
+        // Return the table information
+        return $xartable;
+    }
+}
